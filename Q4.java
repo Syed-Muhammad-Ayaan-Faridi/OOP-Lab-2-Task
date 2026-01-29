e81f976ec068743f4238a594ab87d0c1c2f60c07
Syed Muhammad Ayaan Faridi <k253103@nu.edu.pk>
2026-01-29 10:52:49 +0500

    Q4.java

---
+++ Q4.java
@@ -0,0 +1,9 @@
+package com.SMAF;
+
+public class Lec1 {
+    public static void main(String[] args) {
+    double ans = 0;
+    ans = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+    System.out.println("Final Answer : "+ans);
+    }
+}
